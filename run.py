c3c1c59768422329d4cdcee59350252e807b8a7b
root <root@ih1176054.vds.myihor.ru>
2018-09-09 12:06:46 +0300

    some changes

--- run.py
+++ run.py
@@ -1,17 +1,16 @@
-from flask import Flask, render_template, send_from_directory
+from flask import   Flask, render_template, send_from_directory, \
+                    redirect, request, Response, stream_with_context, \
+                    make_response, flash, url_for, send_from_directory
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "used car project"
+def index():
+    return render_template('index.tmpl')
 
 @app.errorhandler(404)
 def not_found(error):
-    return render_template('404.html'), 404
-
-@app.route('/a')
-def a():
-    return render_template('index.tmpl')
+    return render_template('404.html')
 
 
 @app.route("/test", methods=["POST"])
@@ -19,12 +18,6 @@ def test():
     return _test(request.form["test"])
 
 
-@app.route("/index")
-def index():
-    return _test("My Test Data")
-
-
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', 
-            debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
 
