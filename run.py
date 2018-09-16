3b37fde5bd375fd0b43b43549d70dc32c6709798
Maxim Klyuev <max@2327.ru>
2018-09-16 17:44:33 +0300

    fix

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ from flask import   Flask, render_template, send_from_directory, \
                     redirect, request, Response, stream_with_context, \
                     make_response, flash, url_for, send_from_directory
 import os 
+from dbapi.dbtools import Data_Getter
 
 app = Flask(__name__)
 
@@ -10,18 +11,20 @@ app = Flask(__name__)
 def index():
     return render_template('index.tmpl')
 
+
+@app.route("/search", methods=["POST"])
+def search():
+    return render_template("search.tmpl", result='666')
+
+
 @app.route('/<path:path>')
 def static_file(path):
     return app.send_static_file(path)
 
+
 @app.errorhandler(404)
 def not_found(error):
     return render_template('404.html'), 404
-
-
-@app.route("/test", methods=["POST"])
-def test():
-    return _test(request.form["test"])
 
 
 if __name__ == "__main__":
