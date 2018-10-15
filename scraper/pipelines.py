d4c8c3a861cf511294f7c1658dd8af6a7eac343c
vicsergev <v.s.sergevnin@gmail.com>
2018-10-16 00:44:38 +0300

    Added proxy and scraping full engine information

--- scraper/pipelines.py
+++ scraper/pipelines.py
@@ -10,25 +10,25 @@ from dbapi.dbtools import Base_Updater
 
 class CarsScraperPipeline(object):
 
-    # def __init__(self):
-    #     self.updater = Base_Updater()
-    #
-    # def open_spider(self, spider):
-    #     self.updater.start_updating()
-    #
-    # def process_item(self, item, spider):
-    #     print(item.keys(), item.values())
-    #     self.updater.update(item)
-    #     return item
-    #
-    # def close_spider(self, spider):
-    #     print('close_spider() method run...')
-    #     self.updater.end_updating()
+    def __init__(self):
+        self.updater = Base_Updater()
 
-    # DEBUG
+    def open_spider(self, spider):
+        self.updater.start_updating()
+
     def process_item(self, item, spider):
-        print(item)
+        print(item.keys(), item.values())
+        self.updater.update(item)
         return item
 
     def close_spider(self, spider):
-        print('Closing...')
+        print('close_spider() method run...')
+        self.updater.end_updating()
+
+    # DEBUG
+    # def process_item(self, item, spider):
+    #     print(item)
+    #     return item
+    #
+    # def close_spider(self, spider):
+    #     print('Closing...')
